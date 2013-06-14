fd46848bdcfaa7eedbbf179acfaa6689bf8a9d24
Ryan Kaulakis <rmk216@ist.psu.edu>
2013-06-14 12:37:55 -0400

    fixed

--- trunk/resources/amt_util_testing_clearkeys.sql
+++
@@ -1,8 +0,0 @@
----This is just a utility I used a lot while testing the AMT keys.
----Don't run it on a produciton server, since it will wipe everything AMT-related
-
-delete from user where amt_visitor='1';
-
-delete from `amtkey`;
-
-insert into `amtkey` (`key`, `expiration`, `created`) values ('justtesting', '72036854775807', '19830905132800');
